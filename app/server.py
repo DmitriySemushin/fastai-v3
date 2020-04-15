f14064f50dd2c936a8df4290c1f77750a611b037
DmitriySemushin <36043949+DmitriySemushin@users.noreply.github.com>
2020-04-16 03:15:40 +0400

    Change model download path and name

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1TkE7BEQRweOi1LGjGYmzSB2yIro4NlEI'
+export_file_name = 'bears_clf.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
